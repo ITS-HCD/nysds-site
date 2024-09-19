f4043d38980443b5a371dd44da058185fe360e3b
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2024-09-19 15:31:53 -0400

    url fixes

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
     buildDate: new Date(),
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https:its-hcd.github.io/excelsior-site',
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://its-hcd.github.io/excelsior-site',
     title: process.env.ELEVENTY_ENV === 'development' ? '🔴 Excelsior Design System' : 'Excelsior Design System',
     description: "Guidelines, components, and tools for building digital services in New York State.",
 };
