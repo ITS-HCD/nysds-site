95349f8222e6ed57815a30d8596ca72b6b20f24e
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2024-12-12 10:05:04 -0500

    try to enable noindex globally

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,4 +3,5 @@ module.exports = {
     url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://its-hcd.github.io/excelsior-site/',
     title: process.env.ELEVENTY_ENV === 'development' ? '🔴 Excelsior Design System' : 'Excelsior Design System',
     description: "Guidelines, components, and tools for building digital services in New York State.",
+    crawl: false,
 };
