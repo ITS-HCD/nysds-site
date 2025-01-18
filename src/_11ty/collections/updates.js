4fc2fe6845fdf064491f3f5f09d9427f3213c0cd
Jesse Gardner <jesse@plasticmind.com>
2025-01-17 20:16:38 -0500

    Added component content type

--- src/_11ty/collections/updates.js
+++ src/_11ty/collections/updates.js
@@ -1,6 +1,9 @@
 module.exports = function(collection) {
     return collection
         .getFilteredByGlob("./src/content/updates/*.md")
+        // Exclude drafts
+        .filter((item) => !item.data.draft)
+        // Updates are sorted by year, descending
         .sort((a, b) => {
             let yearA = parseInt(a.data.year, 10);
             let yearB = parseInt(b.data.year, 10);
