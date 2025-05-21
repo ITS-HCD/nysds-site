0382d9b6b30db44f3ad8e6ee1f041458ecd6edf6
NovaCat35 <bergintonatcapital@gmail.com>
2025-05-21 16:26:58 -0400

    Finish modify all examples code preview in Alert page

--- src/assets/js/nysds-site.js
+++ src/assets/js/nysds-site.js
@@ -5,7 +5,7 @@ const showAlert = () => {
     // Clear previous instance if present
     container.innerHTML = "";
     const newAlert = document.createElement("nys-alert");
-    newAlert.setAttribute("theme", "info");
+    newAlert.setAttribute("type", "info");
     newAlert.setAttribute("heading", "Information status");
     newAlert.setAttribute("text", "This alert will disappear after 3 seconds.");
     newAlert.setAttribute("duration", 3000);
